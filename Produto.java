091738400912af5c8b38b4a8501be65498cf2a46
matheusceub26 <166131363+matheusceub26@users.noreply.github.com>
2025-09-19 15:18:23 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,35 @@
+public abstract class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+    private Categoria categoria;
+
+    public Produto(int codigo, String nome, double preco, Categoria categoria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public abstract void exibirDetalhes();
+}
