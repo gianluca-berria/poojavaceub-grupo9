ec3ab86872dd05da523054fd77498a9767999a6b
Gianluca <gianluca.berria@sempreceub.com>
2025-09-19 16:52:54 -0300

    Add files via upload

---
+++ Venda.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Venda{
+    private int id;
+    private Date data;
+    private List<ItemVenda> itens;
+    private double valorTotal; 
+
+    public Venda(int id, Date data){
+        this.id = id;
+        this.data = data;
+        this.itens = new ArrayList<>();
+        this.valorTotal = 0.0;
+    }
+    public void adicionarProduto(Produto p, int qtd){
+        ItemVenda item = new ItemVenda(p, qtd);
+        this.itens.add(item);
+    }
+    private double calcularTotal(){
+        double total = 0.0;
+        for (ItemVenda item : itens){
+            total += item.calcularSubtotal();
+        }
+        this.valorTotal = total;
+        return total;
+    }
+    public double getValorTotal(){
+        return calcularTotal();
+    }
+}
